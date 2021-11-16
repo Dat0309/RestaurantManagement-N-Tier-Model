070e0add3f7706ec960e290c6ceeac8893b7251a
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-11-16 15:12:44 +0700

    Account DataAccess

---
+++ QuanLyDangKyHocPhan/DataAccess/AccountDA.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class AccountDA
+    {
+       public List<Account> GetAll()
+       {
+            SqlConnection conn = new SqlConnection(Ultilities.ConnectionString);
+            conn.Open();
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = Ultilities.Account_GetAll;
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Account> list = new List<Account>();
+            while (reader.Read())
+            {
+                Account account = new Account();
+                account.AccountName = reader["AccountName"].ToString();
+                account.Password = reader["Password"].ToString();
+                account.FullName = reader["FullName"].ToString();
+                account.Email = reader["Email"].ToString();
+                account.Tell = reader["Tell"].ToString();
+                account.DateCreate = DateTime.Parse(reader["DateCreated"].ToString());
+            }
+
+            conn.Close();
+            return list;
+       }
+
+        public int Insert_Update_Delete(Account account, int action)
+        {
+            SqlConnection conn = new SqlConnection(Ultilities.ConnectionString);
+            conn.Open();
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = Ultilities.Category_InsertUpdateDelete;
+
+            cmd.Parameters.Add("@AccountName", SqlDbType.NVarChar, 200).Value = account.AccountName;
+            cmd.Parameters.Add("@Password", SqlDbType.NVarChar, 200).Value = account.Password;
+            cmd.Parameters.Add("@FullName", SqlDbType.NVarChar, 200).Value = account.FullName;
+            cmd.Parameters.Add("@Email", SqlDbType.NVarChar, 200).Value = account.Email;
+            cmd.Parameters.Add("@Tell", SqlDbType.NVarChar, 200).Value = account.Tell;
+            cmd.Parameters.Add("@DateCreate", SqlDbType.NVarChar, 200).Value = account.DateCreate;
+
+            cmd.Parameters.Add("@Action", SqlDbType.Int).Value = action;
+
+            int result = cmd.ExecuteNonQuery();
+            if (result > 0)
+            {
+                return 1;
+            }
+            return 0;
+
+            conn.Close();
+        }
+    }
+}
---
+++ QuanLyDangKyHocPhan/DataAccess/Ultilities.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DataAccess
+{
+    public class Ultilities
+    {
+        private static string StrName = "ConnectionStringName";
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings
+            [StrName].ConnectionString;
+
+        public static string Account_GetAll = "Account_GetAll";
+        public static string Category_GetAll = "Category_GetAll";
+        public static string Food_GetAll = "Food_GetAll";
+        public static string BillDetails_GetAll = "BillDetails_GetAll";
+        public static string Bills_GetAll = "Bills_GetAll";
+        public static string Role_GetAll = "Role_GetAll";
+        public static string RoleAcc_GetAll = "RoleAcc_GetAll";
+        public static string Table_GetAll = "Table_GetAll";
+
+        public static string Category_InsertUpdateDelete = "Category_InsertUpdateDelete";
+        public static string Food_InsertUpdateDelete = "Food_InsertUpdateDelete";
+        public static string Account_InsertUpdateDelete = "Account_InsertUpdateDelete";
+        public static string BillDetails_InsertUpdateDelete = "BillDetails_InsertUpdateDelete";
+        public static string Bills_InsertUpdateDelete = "Bills_InsertUpdateDelete";
+        public static string Role_InsertUpdateDelete = "Role_InsertUpdateDelete";
+        public static string RoleAcc_InsertUpdateDelete = "RoleAcc_InsertUpdateDelete";
+        public static string Table_InsertUpdateDelete = "Table_InsertUpdateDelete";
+    }
+}
