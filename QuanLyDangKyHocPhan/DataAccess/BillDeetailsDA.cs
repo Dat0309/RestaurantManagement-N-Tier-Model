68c73b7d42a6e49ec970c989d5114fb6770f519a
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-11-16 15:43:23 +0700

    Billdetail Dataaccess

---
+++ QuanLyDangKyHocPhan/DataAccess/BillDeetailsDA.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class BillDeetailsDA
+    {
+        public List<BillDetails> GetAll()
+        {
+            SqlConnection conn = new SqlConnection(Ultilities.ConnectionString);
+            conn.Open();
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = Ultilities.BillDetails_GetAll;
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<BillDetails> list = new List<BillDetails>();
+            while (reader.Read())
+            {
+                BillDetails bd = new BillDetails();
+                bd.Id = Convert.ToInt32(reader["ID"]);
+                bd.InvoiceId = Convert.ToInt32(reader["InvoiceID"]);
+                bd.FoodId = Convert.ToInt32(reader["FoodID"]);
+                bd.Quantity = Convert.ToInt32(reader["Quantity"]);
+                list.Add(bd);
+            }
+
+            conn.Close();
+            return list;
+        }
+
+        public int Insert_Update_Delete(BillDetails billDetails, int action)
+        {
+            SqlConnection conn = new SqlConnection(Ultilities.ConnectionString);
+            conn.Open();
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = Ultilities.BillDetails_InsertUpdateDelete;
+
+            SqlParameter IDPara = new SqlParameter("@ID", SqlDbType.Int);
+            IDPara.Direction = System.Data.ParameterDirection.InputOutput;
+            cmd.Parameters.Add(IDPara).Value = billDetails.Id;
+
+            cmd.Parameters.Add("@InvoiceID", SqlDbType.Int).Value = billDetails.InvoiceId;
+            cmd.Parameters.Add("@FoodID", SqlDbType.Int).Value = billDetails.FoodId;
+            cmd.Parameters.Add("@Quantity", SqlDbType.Int).Value = billDetails.Quantity;
+            cmd.Parameters.Add("@Action", SqlDbType.Int)
+                .Value = action;
+
+            int result = cmd.ExecuteNonQuery();
+            if (result > 0)
+            {
+                return (int)cmd.Parameters["@ID"].Value;
+            }
+            return 0;
+
+            conn.Close();
+        }
+    }
+}
---
+++ QuanLyDangKyHocPhan/DataAccess/BillDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class BillDetails
+    {
+        public int Id { get; set; }
+        public int InvoiceId { get; set; }
+        public int FoodId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
